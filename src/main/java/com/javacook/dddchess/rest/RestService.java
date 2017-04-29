45ccdd4be33f92b3f8a7eb09cb286c867a60b5e8
javacook <javacook@gmx.de>
2017-04-30 00:31:32 +0200

    Two new methods

--- src/main/java/com/javacook/dddchess/rest/RestService.java
+++ src/main/java/com/javacook/dddchess/rest/RestService.java
@@ -5,8 +5,11 @@ import akka.dispatch.OnComplete;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 import com.javacook.dddchess.api.ChessGameApi;
+import com.javacook.dddchess.domain.FigureValueObject;
 import com.javacook.dddchess.domain.MoveValueObject;
 import com.javacook.dddchess.domain.PositionValueObject;
+import com.javacook.dddchess.domain.PositionValueObject.HorCoord;
+import com.javacook.dddchess.domain.PositionValueObject.VertCoord;
 import org.glassfish.jersey.server.ManagedAsync;
 import scala.concurrent.Future;
 
@@ -17,34 +20,69 @@ import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Date;
 import java.util.HashMap;
+
 
 @Path("/chessgame")
 public class RestService {
 
-    @Context ActorSystem actorSystem;
+    @Context
+    ActorSystem actorSystem;
     @Context
     ChessGameApi chessGameApi;
     LoggingAdapter log;
+
 
     @PostConstruct
     void initialize() {
         log = Logging.getLogger(actorSystem, this);
     }
 
+
     @GET
+    @Path("isalive")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String figureAt() {
+        log.info("dddchess is alive");
+        return "DDD-Chess is alive: " + new Date();
+    }
+
+
+    @GET
+    @Path("board")
+    @Produces(MediaType.APPLICATION_JSON)
+    public FigureValueObject figureAt(@QueryParam("horCoord") HorCoord horCoord,
+                                      @QueryParam("vertCoord") VertCoord vertCoord) {
+
+        log.info("Get figure at horCoord={}, vertCoord={}", horCoord, vertCoord);
+        return chessGameApi.figureAt(new PositionValueObject(horCoord, vertCoord));
+    }
+
+
+    @POST
     @Path("move")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
     @ManagedAsync
-    public void getExamples (
-            @QueryParam("from") String from,
-            @QueryParam("to") String to,
-            @Suspended final AsyncResponse resp) {
+    public void postMove(@FormParam("move") String move,
+                         @Suspended final AsyncResponse resp) {
 
-        log.info("Try to move from " + from + " to " + to);
+        postMove(new MoveValueObject(move), resp);
+    }
+
+
+    @POST
+    @Path("move")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ManagedAsync
+    public void postMove(MoveValueObject move,
+                         @Suspended final AsyncResponse resp) {
+
+        log.info("Try to perform the move {}", move);
 
         // API-Call:
-        final MoveValueObject move = new MoveValueObject(new PositionValueObject(from), new PositionValueObject(to));
         final Future<Object> future = chessGameApi.move(move);
 
         future.onComplete(new OnComplete<Object>() {
@@ -64,6 +102,6 @@ public class RestService {
                 }
             }
         }, actorSystem.dispatcher());
+    }// move
 
-    }
 }
