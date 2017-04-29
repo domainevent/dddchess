11a4fcf6104f116da2d068dcca151c934079f27b
javacook <javacook@gmx.de>
2017-04-30 00:26:25 +0200

    Two new methods added

--- src/main/java/com/javacook/dddchess/api/ChessGameApiImpl.java
+++ src/main/java/com/javacook/dddchess/api/ChessGameApiImpl.java
@@ -4,11 +4,17 @@ import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 import akka.pattern.Patterns;
 import akka.util.Timeout;
+import com.javacook.dddchess.domain.FigureValueObject;
 import com.javacook.dddchess.domain.MoveValueObject;
 import com.javacook.dddchess.domain.MoveCommand;
+import com.javacook.dddchess.domain.PositionValueObject;
 import scala.concurrent.duration.Duration;
 
 import scala.concurrent.Future;
+
+import static com.javacook.dddchess.domain.FigureValueObject.ColorEnum.BLACK;
+import static com.javacook.dddchess.domain.FigureValueObject.FigureEnum.KING;
+
 
 /**
  * Created by vollmer on 21.04.17.
@@ -25,6 +31,18 @@ public class ChessGameApiImpl implements ChessGameApi {
         ActorSelection chessGameActor = actorSystem.actorSelection("/user/chessGame");
         Timeout timeout = new Timeout(Duration.create(2, "seconds"));
         return Patterns.ask(chessGameActor, new MoveCommand(move), timeout);
+    }
+
+
+    @Override
+    public MoveValueObject getMove(int index) {
+        return null;
+    }
+
+
+    @Override
+    public FigureValueObject figureAt(PositionValueObject position) {
+        return new FigureValueObject(KING, BLACK);
     }
 
 }
