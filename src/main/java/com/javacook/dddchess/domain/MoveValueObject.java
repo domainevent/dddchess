72304d36a27b828db2cd0a34ddc2019a762389f3
javacook <javacook@gmx.de>
2017-04-30 00:29:25 +0200

    Further constructor that takes a String like a2-f4 as argument

--- src/main/java/com/javacook/dddchess/domain/MoveValueObject.java
+++ src/main/java/com/javacook/dddchess/domain/MoveValueObject.java
@@ -1,14 +1,31 @@
 package com.javacook.dddchess.domain;
 
+import javax.xml.bind.annotation.XmlType;
+
+
+@XmlType
 public class MoveValueObject {
 
     public final PositionValueObject from;
     public final PositionValueObject to;
 
 
+    public MoveValueObject() {
+        this((PositionValueObject)null, (PositionValueObject)null);
+    }
+
+
     public MoveValueObject(PositionValueObject from, PositionValueObject to) {
         this.from = from;
         this.to = to;
+    }
+
+    public MoveValueObject(String from, String to) {
+        this(new PositionValueObject(from), new PositionValueObject(to));
+    }
+
+    public MoveValueObject(String fromTo) {
+        this(fromTo.split("-")[0], fromTo.split("-")[1]);
     }
 
     @Override
