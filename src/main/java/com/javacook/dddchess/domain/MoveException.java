408e5059da4e12a91c06b61d33a8da080841a674
javacook <javacook@gmx.de>
2017-05-02 00:04:51 +0200

    new Constructor added

--- src/main/java/com/javacook/dddchess/domain/MoveException.java
+++ src/main/java/com/javacook/dddchess/domain/MoveException.java
@@ -4,4 +4,11 @@ package com.javacook.dddchess.domain;
  * Created by javacook on 30.04.17.
  */
 public class MoveException extends Exception {
+
+    public MoveException() {
+    }
+
+    public MoveException(String message) {
+        super(message);
+    }
 }
