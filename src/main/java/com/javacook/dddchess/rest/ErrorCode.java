e429e191f93a538aa39a45857f0ff8da32fbb3c9
javacook <javacook@gmx.de>
2017-05-04 07:09:06 +0200

    New entry TIMEOUT

--- src/main/java/com/javacook/dddchess/rest/ErrorCode.java
+++ src/main/java/com/javacook/dddchess/rest/ErrorCode.java
@@ -4,5 +4,9 @@ package com.javacook.dddchess.rest;
  * Created by vollmer on 02.05.17.
  */
 public enum ErrorCode {
-    INVALID_MOVE
+
+    TIMEOUT,
+    INVALID_MOVE;
+
+    public final static String ERROR_CODE_KEY = "error code";
 }
