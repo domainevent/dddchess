059e19e0bad7354a49ce2d1f4e45bed5dc459a0e
javacook <javacook@gmx.de>
2017-05-07 08:33:24 +0200

    toString implemented

--- src/main/java/com/javacook/dddchess/domain/ChessBoardValueObject.java
+++ src/main/java/com/javacook/dddchess/domain/ChessBoardValueObject.java
@@ -1,14 +1,27 @@
 package com.javacook.dddchess.domain;
 
+import static com.javacook.dddchess.domain.PositionValueObject.VertCoord._1;
+
+
 /**
  * Created by vollmer on 05.05.17.
  */
-public class ChessBoardValueObject {
+public class ChessBoardValueObject extends ValueObject {
 
     public final FigureValueObject[][] board;
-
 
     public ChessBoardValueObject(FigureValueObject[][] board) {
         this.board = board;
     }
+
+    public ChessBoardValueObject() {
+        this(null);
+        throw new IllegalStateException("This constructor should not be called here.");
+    }
+
+    @Override
+    public String toString() {
+        return ChessBoardEntity.boardToString(board);
+    }
+
 }
