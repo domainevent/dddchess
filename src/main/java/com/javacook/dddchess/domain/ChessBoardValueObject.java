0d1d3fd0dc09757292574f803758ab7648951b87
javacook <javacook@gmx.de>
2017-05-10 06:17:51 +0200

    New method "getBoard"

--- src/main/java/com/javacook/dddchess/domain/ChessBoardValueObject.java
+++ src/main/java/com/javacook/dddchess/domain/ChessBoardValueObject.java
@@ -1,6 +1,8 @@
 package com.javacook.dddchess.domain;
 
-import static com.javacook.dddchess.domain.PositionValueObject.VertCoord._1;
+import com.javacook.dddchess.domain.PositionValueObject.*;
+
+import java.util.Arrays;
 
 
 /**
@@ -8,15 +10,27 @@ import static com.javacook.dddchess.domain.PositionValueObject.VertCoord._1;
  */
 public class ChessBoardValueObject extends ValueObject {
 
-    public final FigureValueObject[][] board;
+    protected final FigureValueObject[][] board;
 
     public ChessBoardValueObject(FigureValueObject[][] board) {
         this.board = board;
     }
 
     public ChessBoardValueObject() {
-        this(null);
+        this(new FigureValueObject[HorCoord.values().length][VertCoord.values().length]);
     }
+
+    public FigureValueObject[][] getBoard() {
+        final FigureValueObject[][] copy =
+                new FigureValueObject[HorCoord.values().length][VertCoord.values().length];
+        for (int i = 0; i < HorCoord.values().length; i++) {
+            for (int j = 0; j < VertCoord.values().length; j++) {
+                copy[i][j] = board[i][j];
+            }
+        }
+        return copy;
+    }
+
 
     @Override
     public String toString() {
